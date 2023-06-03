8bb1b7ceb458cd93b115f2516b31a73bb8b4bd46
anisarif <anis.arif@code.berlin>
2023-06-03 09:50:23 +0200

    config test 1.2

--- Backend/auth.py
+++ Backend/auth.py
@@ -1,6 +1,6 @@
 from flask import request, Blueprint, jsonify, make_response
 from .models import db, UserHome
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, create_refresh_token,jwt_required, get_jwt_identity
 from werkzeug.security import check_password_hash, generate_password_hash
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -50,7 +50,17 @@ def login():
             is_admin = False
             
         access_token = create_access_token(identity=user.id, additional_claims={"is_administrator": is_admin, "current_user":current_user})
-        return jsonify(access_token=access_token)
+        refresh_token = create_refresh_token(identity=user.id, additional_claims={"is_administrator": is_admin, "current_user":current_user})
+        return jsonify(access_token=access_token, refresh_token=refresh_token)
 
     return error
+
+# Refresh Token Route
+
+@bp.route("/refresh", methods=["POST"])
+@jwt_required(refresh=True)
+def refresh():
+    identity = get_jwt_identity()
+    access_token = create_access_token(identity=identity)
+    return jsonify(access_token=access_token)
 
