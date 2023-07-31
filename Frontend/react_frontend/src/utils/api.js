b5be48ae015fc200957f35f8e835bca122f2b63b
anisarif <anis.arif@code.berlin>
2023-07-31 17:37:42 +0200

    update ip

--- Frontend/react_frontend/src/utils/api.js
+++ Frontend/react_frontend/src/utils/api.js
@@ -1,25 +1,3 @@
-
-export const getAccurators = () => {
-    return fetch('http://192.168.178.171:5000/')
-        .then(res => res.json())
-        .catch(error => console.log(error))
-}
-
-
-export const getCurrentWeather = () => {
-    return fetch('http://192.168.178.171:5000/Weather/')
-        .then(res => res.json())
-        .catch(error => console.log(error))
-}
-
-export const getAcc = () => {
-    return fetch('http://192.168.178.171:5000/dbtest/')
-        .then(res => res.json())
-        .catch(error => console.log(error))
-}
-
-
-
 export const getUsers = () => {
     return fetch('https://197.240.120.86:5000/api/user/getall')
         .then(res => res.json())
